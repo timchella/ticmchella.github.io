940f957c037c3c727b9393fe273b1db7acee534c
timchella <56709778+timchella@users.noreply.github.com>
2023-04-11 17:05:07 -0500

    Add files via upload

--- js/notes_screen.js
+++ js/notes_screen.js
@@ -1,11 +1,21 @@
 
 window.addEventListener('load', function() {
     backButton = document.getElementById("back-button");
-    console.log(backButton);
+    // console.log(backButton);
     backButton.addEventListener("click", () => {
         currentURL = window.location.protocol + "//" + window.location.host;
         // console.log(currentURL);
         newURL = currentURL + "/home_screen.html";
+        // console.log(newURL);
+        window.location.href = newURL;
+    });
+
+    cameraRollLink = document.getElementById("camera-roll-link");
+    // console.log(cameraRollLink);
+    cameraRollLink.addEventListener("click", () => {
+        currentURL = window.location.protocol + "//" + window.location.host;
+        // console.log(currentURL);
+        newURL = currentURL + "/camera_roll.html";
         // console.log(newURL);
         window.location.href = newURL;
     });
